8c42f29cf585fe417cd274fcfe7bb9fc9907a1f4
Erick Garcia <20233tn149@utez.edu.mx>
2025-11-05 21:34:00 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    assetsInlineLimit: 0
   }
 })
